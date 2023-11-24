ffc9af855fc881e534a269c294415df7024983ed
Felix <75081082+vectorworm@users.noreply.github.com>
2023-11-24 17:46:39 +0100

    stuff

--- js/autoload_nav.js
+++ js/autoload_nav.js
@@ -13,3 +13,16 @@ function inflateLogo(){
   document.getElementById("logo").src = "/img/vectorworm-inflate.gif";
   squish_sound.play();
 }
+
+/* toggle between addign and removeing "responsive" class to nav when the user click menu*/
+function toggle_menu(){
+  var x = document.getElementById("nav");
+  if (x.className === "navigation"){
+    x.className += " responsive";
+    document.getElementById("menu_button").src = "/img/menu_close.png";
+  } else {
+    x.className = "navigation";
+    document.getElementById("menu_button").src = "/img/menu.png";
+  }
+
+}
